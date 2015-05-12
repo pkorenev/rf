acef1408f77e1238dde2abfafef5eb9b51b8b506
VoroninNick <p.korenev@voroninstudio.eu>
2015-05-12 18:00:08 +0300

    wizard images

--- app/controllers/wizard_controller.rb
+++ app/controllers/wizard_controller.rb
@@ -5,12 +5,28 @@ class WizardController < ApplicationController
 
   def ng_wizard
     @platforms =  [
-        {name: 'Browsers', sub_items: [
+        {
+            name: 'Browsers', image_path: "ie.svg", sub_items: [
             {name: 'Internet Explorer 9', count: 0},
             {name: 'Internet Explorer 10', count: 0},
+            {name: 'Internet Explorer 11', count: 0},
             {name: 'Latest version of Firefox', count: 0},
-            {name: 'Latest version of Chrome', count: 0}
+            {name: 'Latest version of Chrome', count: 0},
+            {name: 'Latest version of Safari', count: 0}
         ]
+        },
+        {
+            name: "IOS", image_path: "apple.svg", sub_items: [
+              { name: "iPhone 4", count: 0 },
+              { name: "iPhone 5s", count: 0 },
+              { name: "iPad 2", count: 0 },
+              { name: "iPad Air", count: 0 },
+              { name: "iPad mini", count: 0 },
+              { name: "iPhone 6 Plus", count: 0 }
+            ]
+        },
+        {
+            name: "Android", image_path: "android.svg"
         }
     ]
   end
